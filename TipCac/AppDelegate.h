112e8a8550bf5ca5ff89ffe7e875efcafca75e8f
Jessica Bai <jessicakfl@Jessicas-MacBook-Pro.local>
2017-03-03 20:05:35 -0800

    Initial Commit

---
+++ TipCac/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TipCac
+//
+//  Created by Jessica Bai on 3/3/17.
+//  Copyright © 2017 Jessica Bai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TipCac/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TipCac
+//
+//  Created by Jessica Bai on 3/3/17.
+//  Copyright © 2017 Jessica Bai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
