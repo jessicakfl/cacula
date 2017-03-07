2d7f586a9dcbc8f67033ba2e423057d6ae23d0ed
Jessica Bai <jessicakfl@Jessicas-MacBook-Pro.local>
2017-03-07 00:11:25 -0800

    add

--- TipCac/ViewController.h
+++ TipCac/ViewController.h
@@ -10,6 +10,20 @@
 
 @interface ViewController : UIViewController
 
+@property (weak, nonatomic) IBOutlet UITextField *purchaseAmond;
+@property (weak, nonatomic) IBOutlet UISlider *sliderPercent;
+- (IBAction)cacul:(UIButton *)sender;
+@property (nonatomic) int cacutlatedTipAmount;
+- (IBAction)segControl:(UISegmentedControl *)sender;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *segCon;
+
+@property (weak, nonatomic) IBOutlet UILabel *plab;
+@property (nonatomic) int purchaseAmount;
+- (IBAction)valueChanged:(UISlider *)sender;
+@property (weak, nonatomic) IBOutlet UILabel *tipAmount;
+- (IBAction)purchaseChange:(UITextField *)sender;
+- (int) cacTip:(float) value: (int) pamount;
+
 
 @end
 
--- TipCac/ViewController.m
+++ TipCac/ViewController.m
@@ -16,7 +16,21 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    [_purchaseAmond addTarget:self action:@selector(purchaseChange:) forControlEvents:UIControlEventEditingChanged];
+//    UIScrollView *scroll = [[UIScrollView alloc] initWithFrame:CGRectMake(0, 0, 320, 435)];
+//    scroll.contentSize = CGSizeMake(320, 700);
+//    scroll.showsHorizontalScrollIndicator = YES;
+//    
+//    NSArray *itemArray = [NSArray arrayWithObjects: @"One", @"Two", @"Three", nil];
+//    UISegmentedControl *segmentedControl = [[UISegmentedControl alloc] initWithItems:itemArray];
+//    segmentedControl.frame = CGRectMake(35, 200, 250, 50);
+//    segmentedControl.segmentedControlStyle = UISegmentedControlStylePlain;
+//    [segmentedControl addTarget:self action:@selector(MySegmentControlAction:) forControlEvents: UIControlEventValueChanged];
+//    segmentedControl.selectedSegmentIndex = 1;
+//    [scroll addSubview:segmentedControl];
+//    //[segmentedControl release];
+//    [self.view addSubview:scroll];
+
 }
 
 
@@ -26,4 +40,26 @@
 }
 
 
+- (IBAction)purchaseChange:(UITextField *)sender {
+    NSString *amount =[sender text];
+    self.purchaseAmount=[amount intValue];
+    [self cacTip:self.sliderPercent.value :_purchaseAmount];
+    self.tipAmount.text=[NSString stringWithFormat:@"%d",self.cacutlatedTipAmount];
+}
+- (IBAction)valueChanged:(UISlider *)sender {
+    [self cacTip:self.sliderPercent.value:self.purchaseAmount];
+    float value = [sender value];
+    self.plab.text = [NSString stringWithFormat:@"%f%%",value];
+    int tip=[self cacTip:self.sliderPercent.value :_purchaseAmount];
+    self.tipAmount.text=[NSString stringWithFormat:@"%d",tip];
+}
+- (int) cacTip:(float) value: (int) pamount{
+    return value * pamount;
+}
+- (IBAction)segControl:(UISegmentedControl *)sender {
+}
+- (IBAction)cacul:(UIButton *)sender {
+//    int tip=[self cacTip:self.sliderPercent.value :_purchaseAmount];
+//    self.tipAmount.text=[NSString stringWithFormat:@"%d",tip];
+}
 @end
